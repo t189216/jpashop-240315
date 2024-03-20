ae1d730c3749aa6060920d52e1859296048f9583
seoyeon <tus0926@naver.com>
2024-03-21 01:25:35 +0900

    홈 화면 & 타임리프 뷰

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
