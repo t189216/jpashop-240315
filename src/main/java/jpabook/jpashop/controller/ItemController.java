2d63e557aa139e037b21303c7738555f188d093b
seoyeon <tus0926@naver.com>
2024-03-21 04:25:54 +0900

    상품 목록

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -2,11 +2,14 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.Service.ItemService;
 import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.domain.Item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,5 +35,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
